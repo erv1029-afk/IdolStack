2379d130dee96b6c6ef61b9d7d19743fac6d1278
Emily Van Tries <erv1029@gmail.com>
2025-11-22 14:11:01 -0500

    fixed youtube paths

--- IdolStack/frontend/src/data/groupData.js
+++ IdolStack/frontend/src/data/groupData.js
@@ -2,47 +2,47 @@ const groupData = [
   {
     name: "BTS",
     color: "#A2D2FF", // soft blue
-    youtube: "https://www.youtube.com/@BANGTANTV",
+    youtube: "https://www.youtube.com/channel/UC3IZKseVpdzPSBaWxBxundA", // BANGTANTV
   },
   {
     name: "ATEEZ",
     color: "#FFAFCC", // pink coral
-    youtube: "https://www.youtube.com/@ATEEZofficial",
+    youtube: "https://www.youtube.com/channel/UCcFqCkZ9l0wY3zIhY6uY2Zw", // ATEEZ Official
   },
   {
     name: "Stray Kids",
     color: "#BDB2FF", // lavender purple
-    youtube: "https://www.youtube.com/@StrayKids",
+    youtube: "https://www.youtube.com/channel/UC9rMiEjNaCSsebs31MRDCRA", // Stray Kids Official
   },
   {
     name: "ENHYPEN",
     color: "#FFC6FF", // pastel pink
-    youtube: "https://www.youtube.com/@ENHYPEN",
+    youtube: "https://www.youtube.com/c/ENHYPENOFFICIAL", // ENHYPEN Official
   },
   {
     name: "XIKERS",
     color: "#FFD6A5", // warm peach
-    youtube: "https://www.youtube.com/@xikers_official",
+    youtube: "https://www.youtube.com/@xikers_official", // XIKERS Official (handle works, but direct link is fine too)
   },
   {
     name: "BLACKPINK",
     color: "#FFB6C1", // classic pink
-    youtube: "https://www.youtube.com/@BLACKPINK",
+    youtube: "https://www.youtube.com/channel/UCOmHUn--16B90oW2L6FRR3A", // BLACKPINK Official
   },
   {
     name: "XG",
     color: "#CDE7B0", // mint green
-    youtube: "https://www.youtube.com/@XGOfficial",
+    youtube: "https://www.youtube.com/@xg_official", // XG Official
   },
   {
     name: "IVE",
     color: "#FEC89A", // soft orange
-    youtube: "https://www.youtube.com/@IVEstarship",
+    youtube: "https://www.youtube.com/c/IVEstarship", // IVE Official
   },
   {
     name: "aespa",
     color: "#B5EAEA", // aqua blue
-    youtube: "https://www.youtube.com/@aespa",
+    youtube: "https://www.youtube.com/@aespa", // aespa Official
   },
 ];
 
